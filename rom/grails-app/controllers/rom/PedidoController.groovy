3c19c8c63d554ca4e07009f527c4352ea68e201e
Ignacio Garay <imgarayojeda@gmail.com>
2014-05-12 19:16:02 -0300

    Fix a los bugs del service del menu. Fix al bootstrap

--- rom/grails-app/controllers/rom/PedidoController.groovy
+++ rom/grails-app/controllers/rom/PedidoController.groovy
@@ -104,6 +104,11 @@ class PedidoController {
 	
 	
 	@Secured(['permitAll'])
+	def anular(Pedido pedidoInstance) {
+		redirect action:'list'
+	}
+	
+	@Secured(['permitAll'])
 	@Transactional(readOnly = false)
 	def pago(long idRestaurant, long idMesa) {
 		Mesa mesa = Mesa.findByIdAndRestaurant(idMesa, Restaurant.findById(idRestaurant))
